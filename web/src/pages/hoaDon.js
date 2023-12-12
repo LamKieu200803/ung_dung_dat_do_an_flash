0d6968e4716cdc6eb3c919ae3a2b7bee2777a719
bfjshfbh <nhatltph28401@fpt.edu.vn>
2023-12-12 20:20:22 +0700

    Update hoaDon.js

--- web/src/pages/hoaDon.js
+++ web/src/pages/hoaDon.js
@@ -5,9 +5,14 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedOrderId, setSelectedOrderId] = useState(null);
+  const [newStatus, setNewStatus] = useState("");
+  const [filterStatus, setFilterStatus] = useState("Tất cả");
 
   const fetchData = async () => {
     try {
@@ -21,6 +26,40 @@ const Orders = () => {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const handleStatusUpdate = async () => {
+    try {
+      const res = await axios.put(
+        `http://localhost:9997/hoadon/sua/${selectedOrderId.userId}/${selectedOrderId._id}`,
+        { trangthai: newStatus }
+      );
+      console.log(res.data);
+      setShowModal(false);
+      fetchData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleOpenModal = (orderId) => {
+    setShowModal(true);
+    setSelectedOrderId(orderId);
+    setNewStatus(""); // Reset newStatus when opening the modal
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setSelectedOrderId(null);
+  };
+
+  const handleFilterStatusChange = (status) => {
+    setFilterStatus(status);
+  };
+
+  const filteredOrders =
+    filterStatus === "Tất cả"
+      ? orders
+      : orders.filter((order) => order.trangthai === filterStatus);
 
   const columns = [
     { name: "ID", selector: (row, index) => `#${index + 1}` },
@@ -54,19 +93,65 @@ const Orders = () => {
       selector: (row) => row.trangthai,
       sortable: true,
     },
+    {
+      name: "Xem chi tiết",
+      cell: (row) => (
+        <Button variant="outline-warning" onClick={() => handleOpenModal(row)}>
+          Sửa trạng thái
+        </Button>
+      ),
+    },
   ];
 
   return (
     <div style={{ padding: "20px 50px" }}>
+      <Dropdown onSelect={(status) => handleFilterStatusChange(status)}>
+        <Dropdown.Toggle variant="info" id="dropdown-basic">
+          {filterStatus}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+<Dropdown.Item eventKey="Tất cả">Tất cả</Dropdown.Item>
+          <Dropdown.Item eventKey="Đang xử lý">Đang xử lý</Dropdown.Item>
+          <Dropdown.Item eventKey="Đã giao">Đã giao</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
       <DataTable
         columns={columns}
-        data={orders}
+        data={filteredOrders}
         pagination
         paginationPerPage={5}
         striped
         subHeader
         subHeaderAlign="left"
       />
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Cập nhật trạng thái</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group as={Col}>
+            <Form.Label>Trạng thái mới</Form.Label>
+            <Dropdown onSelect={(status) => setNewStatus(status)}>
+              <Dropdown.Toggle variant="info" id="dropdown-basic">
+                {newStatus || "Chọn trạng thái"}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="Đang xử lý">Đang xử lý</Dropdown.Item>
+                <Dropdown.Item eventKey="Đã giao">Đã giao</Dropdown.Item>
+                {/* Add other status options as needed */}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Đóng
+          </Button>
+          <Button variant="primary" onClick={handleStatusUpdate}>
+            Cập nhật
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
