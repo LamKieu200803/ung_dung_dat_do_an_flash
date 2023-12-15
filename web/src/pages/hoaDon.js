0c3cef70b8c0e5bb1b6dfdfd56e0b321b11a846a
bfjshfbh <nhatltph28401@fpt.edu.vn>
2023-12-15 20:28:02 +0700

    Update hoaDon.js

--- web/src/pages/hoaDon.js
+++ web/src/pages/hoaDon.js
@@ -1,18 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import DataTable from "react-data-table-component";
 import axios from "axios";
-import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
+import Table from "react-bootstrap/Table";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-  const [selectedOrderId, setSelectedOrderId] = useState(null);
+  const [showStatusModal, setShowStatusModal] = useState(false);
+  const [showDetailModal, setShowDetailModal] = useState(false);
+  const [selectedOrder, setSelectedOrder] = useState(null);
   const [newStatus, setNewStatus] = useState("");
   const [filterStatus, setFilterStatus] = useState("Tất cả");
+  const [originalStatus, setOriginalStatus] = useState("");
 
   const fetchData = async () => {
     try {
@@ -27,40 +28,27 @@ const Orders = () => {
     fetchData();
   }, []);
 
-  const handleStatusUpdate = async () => {
-    try {
-      const res = await axios.put(
-        `http://localhost:9997/hoadon/sua/${selectedOrderId.userId}/${selectedOrderId._id}`,
-        { trangthai: newStatus }
-      );
-      console.log(res.data);
-      setShowModal(false);
-      fetchData();
-    } catch (error) {
-      console.error(error);
-    }
+  const handleCloseStatusModal = () => {
+    setShowStatusModal(false);
+    setSelectedOrder(null);
   };
 
-  const handleOpenModal = (orderId) => {
-    setShowModal(true);
-    setSelectedOrderId(orderId);
-    setNewStatus(""); // Reset newStatus when opening the modal
+  const handleCloseDetailModal = () => {
+    setShowDetailModal(false);
+    setSelectedOrder(null);
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
-    setSelectedOrderId(null);
+  const handleOpenStatusModal = (order) => {
+    setShowStatusModal(true);
+    setSelectedOrder(order);
+    setOriginalStatus(order.trangthai);
+    setNewStatus(order.trangthai);
   };
 
-  const handleFilterStatusChange = (status) => {
-    setFilterStatus(status);
+  const handleOpenDetailModal = (order) => {
+    setShowDetailModal(true);
+    setSelectedOrder(order);
   };
-
-  const filteredOrders =
-    filterStatus === "Tất cả"
-      ? orders
-      : orders.filter((order) => order.trangthai === filterStatus);
-
   const columns = [
     { name: "ID", selector: (row, index) => `#${index + 1}` },
     {
@@ -94,61 +82,148 @@ const Orders = () => {
       sortable: true,
     },
     {
-      name: "Xem chi tiết",
+      name: "Cập nhật trạng thái",
       cell: (row) => (
-        <Button variant="outline-warning" onClick={() => handleOpenModal(row)}>
+        <Button
+          variant="outline-warning"
+          onClick={() => handleOpenStatusModal(row)}
+        >
           Sửa trạng thái
         </Button>
       ),
     },
+    {
+      name: "Xem chi tiết",
+      cell: (row) => (
+        <Button
+          variant="outline-primary"
+          onClick={() => handleOpenDetailModal(row)}
+        >
+          Xem chi tiết
+        </Button>
+      ),
+    },
   ];
+  const handleStatusUpdate = async () => {
+    try {
+      if (!selectedOrder) {
+        console.error("No order selected.");
+        return;
+      }
+      if (newStatus !== originalStatus) {
+        const res = await axios.put(
+`http://localhost:9997/hoadon/sua/${selectedOrder.userId}/${selectedOrder._id}`,
+          { trangthai: newStatus }
+        );
+        console.log(res.data);
+
+        fetchData();
+        setShowStatusModal(false);
+      } else {
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const filteredData = orders.filter((order) => {
+    if (filterStatus === "Tất cả") return true;
+    return order.trangthai === filterStatus;
+  });
 
   return (
     <div style={{ padding: "20px 50px" }}>
-      <Dropdown onSelect={(status) => handleFilterStatusChange(status)}>
-        <Dropdown.Toggle variant="info" id="dropdown-basic">
+      <Dropdown onSelect={(status) => setFilterStatus(status)}>
+        <Dropdown.Toggle variant="primary" id="dropdown-basic">
           {filterStatus}
         </Dropdown.Toggle>
         <Dropdown.Menu>
-<Dropdown.Item eventKey="Tất cả">Tất cả</Dropdown.Item>
+          <Dropdown.Item eventKey="Tất cả">Tất cả</Dropdown.Item>
           <Dropdown.Item eventKey="Đang xử lý">Đang xử lý</Dropdown.Item>
           <Dropdown.Item eventKey="Đã giao">Đã giao</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
       <DataTable
         columns={columns}
-        data={filteredOrders}
+        data={filteredData}
         pagination
         paginationPerPage={5}
         striped
         subHeader
         subHeaderAlign="left"
       />
-      <Modal show={showModal} onHide={handleCloseModal}>
+      <Modal show={showStatusModal} onHide={handleCloseStatusModal}>
         <Modal.Header closeButton>
-          <Modal.Title>Cập nhật trạng thái</Modal.Title>
+          <Modal.Title>Cập nhật trạng thái đơn hàng</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form.Group as={Col}>
-            <Form.Label>Trạng thái mới</Form.Label>
-            <Dropdown onSelect={(status) => setNewStatus(status)}>
-              <Dropdown.Toggle variant="info" id="dropdown-basic">
-                {newStatus || "Chọn trạng thái"}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                <Dropdown.Item eventKey="Đang xử lý">Đang xử lý</Dropdown.Item>
-                <Dropdown.Item eventKey="Đã giao">Đã giao</Dropdown.Item>
-                {/* Add other status options as needed */}
-              </Dropdown.Menu>
-            </Dropdown>
-          </Form.Group>
+          <p>Chọn trạng thái mới:</p>
+          <Dropdown onSelect={setNewStatus}>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              {newStatus}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item eventKey="Đang xử lý">Đang xử lý</Dropdown.Item>
+              <Dropdown.Item eventKey="Đã giao">Đã giao</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
+          <Button variant="secondary" onClick={handleCloseStatusModal}>
             Đóng
           </Button>
           <Button variant="primary" onClick={handleStatusUpdate}>
-            Cập nhật
+            Lưu thay đổi
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showDetailModal} onHide={handleCloseDetailModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Chi tiết hóa đơn</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedOrder && (
+            <div>
+              <p>ID hóa đơn: {selectedOrder._id}</p>
+              <p>Tên khách hàng: {selectedOrder.tennguoimua}</p>
+              <p>Phương thức thanh toán: {selectedOrder.pttt ?? "Tiền mặt"}</p>
+              <p>Địa chỉ: {selectedOrder.diachi}</p>
+              <p>Số điện thoại: {selectedOrder.sdt}</p>
+              <p>Thời gian đặt hàng: {selectedOrder.thoigian}</p>
+              <p>Trạng thái: {selectedOrder.trangthai}</p>
+              <h5>Danh sách sản phẩm:</h5>
+              <Table striped bordered hover>
+                <thead>
+<tr>
+                    <th>Tên sản phẩm</th>
+                    <th>Số lượng</th>
+                    <th>Giá</th>
+                    <th>Ảnh sản phẩm</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {selectedOrder.danhSachSanPham.map((product) => (
+                    <tr key={product._id}>
+                      <td>{product.tensp}</td>
+                      <td>{product.soluongmua}</td>
+                      <td>{product.giasp}</td>
+                      <td>
+                        <img
+                          src={product.img}
+                          alt={product.tensp}
+                          style={{ width: "50px", height: "50px" }}
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDetailModal}>
+            Đóng
           </Button>
         </Modal.Footer>
       </Modal>
