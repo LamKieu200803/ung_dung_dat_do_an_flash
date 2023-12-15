42665eb6df5c563ddb71728c3d71bf92ff03d5d1
bfjshfbh <nhatltph28401@fpt.edu.vn>
2023-12-15 20:28:21 +0700

    Update khachHang.js

--- web/src/pages/khachHang.js
+++ web/src/pages/khachHang.js
@@ -3,35 +3,39 @@ import DataTable from "react-data-table-component";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const KhachHang = () => {
   const [users, setUsers] = useState([]);
   const [detailShow, setDetailShow] = useState(false);
-  const [selectedUser, setSelectedUser] = useState();
-  const [show, setShow] = useState(false);
-  const handleDetailShow = async (rowData) => {
-    const res = await axios.get(
-      "http://localhost:9997/thongtin/" + rowData._id
-    );
-    setSelectedUser(res.data[0]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [showPassword, setShowPassword] = useState({});
 
+  const handleRowClick = (row) => {
+    setSelectedUser(row);
     setDetailShow(true);
   };
+
+  const togglePasswordVisibility = (userId) => {
+    setShowPassword((prevShowPassword) => ({
+      ...prevShowPassword,
+      [userId]: !prevShowPassword[userId],
+    }));
+  };
+
   const fetchData = async () => {
     try {
       const res = await axios.get("http://localhost:9997/user");
-      console.log(res);
       setUsers(res.data);
     } catch (error) {
       console.error(error);
     }
   };
-  const handleShow = () => setShow(true);
+
   useEffect(() => {
     fetchData();
   }, []);
+
   const columns = [
     { name: "ID", selector: (row, index) => `#${index + 1}` },
     {
@@ -41,19 +45,31 @@ const KhachHang = () => {
     },
     {
       name: "Password",
-      selector: (row) => row.password,
+      selector: (row) => (
+        <span style={{ display: "flex", alignItems: "center" }}>
+          {showPassword[row._id] ? row.password : "****"}
+          <Button
+            variant="link"
+            size="sm"
+            style={{ marginBottom: "7px" }}
+            onClick={() => togglePasswordVisibility(row._id)}
+          >
+            {showPassword[row._id] ? <FaEyeSlash /> : <FaEye />}
+          </Button>
+        </span>
+      ),
       sortable: true,
     },
-    ,
     {
       name: "Xem chi tiết",
       cell: (row) => (
-        <Button variant="outline-primary" onClick={() => handleDetailShow(row)}>
+        <Button variant="outline-primary" onClick={() => handleRowClick(row)}>
           Xem chi tiết
         </Button>
       ),
     },
   ];
+
   return (
     <div style={{ padding: "20px 50px" }}>
       <Modal show={detailShow} onHide={() => setDetailShow(false)}>
@@ -63,17 +79,8 @@ const KhachHang = () => {
         <Modal.Body>
           {selectedUser && (
             <div>
-              <img
-                style={{
-                  width: "200px",
-                  height: "150px",
-                  objectFit: "cover",
-                  padding: "10px 10px 10px 0px",
-                }}
-                src={selectedUser?.anh}
-              />
-              <p>Tên người mua: {selectedUser?.tennguoimua}</p>
-              <p>Số điện thoại: 0{selectedUser?.phone}</p>
+              <p>Email: {selectedUser.email}</p>
+              <p>Password: {selectedUser.password}</p>
             </div>
           )}
         </Modal.Body>
@@ -85,6 +92,7 @@ const KhachHang = () => {
         pagination
         paginationPerPage={5}
         striped
+        onRowClicked={handleRowClick}
       />
     </div>
   );
